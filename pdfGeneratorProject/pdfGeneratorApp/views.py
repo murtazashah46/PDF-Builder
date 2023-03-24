ce48557f3b13d442d77806afad1264cff67ca6be
SyedMurtazaHassan <murtazashah46@gmail.com>
2023-03-24 06:43:02 +0500

    Corrected textbox size and added font size,color,align,static fields

--- pdfGeneratorProject/pdfGeneratorApp/views.py
+++ pdfGeneratorProject/pdfGeneratorApp/views.py
@@ -5,79 +5,79 @@ from django.contrib import messages
 import fitz
 
 listOfCoordinates = [
-    {'page_number': 1, 'coordinate_name': "currency", 'x1': 94, 'y1': 126, 'width': 101,
-        'height': 18, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 1, 'coordinate_name': "amount_in_figures", 'x1': 375, 'y1': 126,
-        'width': 186, 'height': 18, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 1, 'coordinate_name': "amount_in_words", 'x1': 252, 'y1': 151,
-        'width': 313, 'height': 18, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 1, 'coordinate_name': "account_number", 'x1': 442, 'y1': 224,
-        'width': 54, 'height': 17, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 1, 'coordinate_name': "beneficiary_name", 'x1': 165, 'y1': 338,
-        'width': 393, 'height': 18, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 1, 'coordinate_name': "beneficiary_address", 'x1': 137, 'y1': 363,
-        'width': 421, 'height': 18, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 1, 'coordinate_name': "IMG_bank_reciept", 'x1': 22, 'y1': 428,
-        'width': 503, 'height': 115, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 1, 'coordinate_name': "currency", 'x1': 90, 'y1': 125, 'width': 35,
+        'height': 18, 'font_size': 15, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 1, 'coordinate_name': "amount_in_figures", 'x1': 375, 'y1': 125,
+        'width': 200, 'height': 18, 'font_size': 15, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 1, 'coordinate_name': "amount_in_words", 'x1': 250, 'y1': 155,
+        'width': 330, 'height': 45, 'font_size': 12, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 1, 'coordinate_name': "account_number", 'x1': 438, 'y1': 225,
+        'width': 60, 'height': 18, 'font_size': 14.8, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 1, 'coordinate_name': "beneficiary_name", 'x1': 165, 'y1': 340,
+        'width': 420, 'height': 18, 'font_size': 12, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 1, 'coordinate_name': "beneficiary_address", 'x1': 137, 'y1': 373,
+        'width': 450, 'height': 30, 'font_size': 8.5, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 1, 'coordinate_name': "IMG_bank_reciept", 'x1': 10, 'y1': 430,
+        'width': 500, 'height': 130, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
 
-    {'page_number': 2, 'coordinate_name': "beneficiary_name", 'x1': 29, 'y1': 108,
-        'width': 215, 'height': 32, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 2, 'coordinate_name': "beneficiary_address", 'x1': 29, 'y1': 144,
-        'width': 215, 'height': 44, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 2, 'coordinate_name': "reference", 'x1': 468, 'y1': 180, 'width': 82,
-        'height': 15, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 2, 'coordinate_name': "date", 'x1': 468, 'y1': 198, 'width': 82,
-        'height': 33, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 2, 'coordinate_name': "due_date", 'x1': 468, 'y1': 216, 'width': 82,
-        'height': 15, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 2, 'coordinate_name': "beneficiary_name", 'x1': 45, 'y1': 90,
+        'width': 215, 'height': 30, 'font_size': 12, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 2, 'coordinate_name': "beneficiary_address", 'x1': 45, 'y1': 120,
+        'width': 215, 'height': 40, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 2, 'coordinate_name': "reference", 'x1': 465, 'y1': 180, 'width': 100,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 2, 'coordinate_name': "date", 'x1': 465, 'y1': 195, 'width': 80,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 2, 'coordinate_name': "due_date", 'x1': 465, 'y1': 210, 'width': 80,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
     {'page_number': 2, 'coordinate_name': "IMG_bank_reciept", 'x1': 72, 'y1': 352,
         'width': 489, 'height': 173, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 2, 'coordinate_name': "currency", 'x1': 309, 'y1': 604, 'width': 72,
-        'height': 22, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 2, 'coordinate_name': "amount_in_figures", 'x1': 396, 'y1': 604,
-        'width': 144, 'height': 22, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 2, 'coordinate_name': "currency", 'x1': 310, 'y1': 605, 'width': 35,
+        'height': 20, 'font_size': 15, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 2, 'coordinate_name': "amount_in_figures", 'x1': 345, 'y1': 605,
+        'width': 200, 'height': 20, 'font_size': 15, 'font_family': 'Arial', 'color': (0, 0, 0)},
 
-    {'page_number': 3, 'coordinate_name': 'beneficiary_name', 'x1': 72, 'y1': 288,
-        'width': 460, 'height': 10, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 3, 'coordinate_name': 'beneficiary_address', 'x1': 72, 'y1': 306,
-        'width': 460, 'height': 21, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 3, 'coordinate_name': 'beneficiary_name', 'x1': 72, 'y1': 280,
+        'width': 470, 'height': 20, 'font_size': 12, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 3, 'coordinate_name': 'beneficiary_address', 'x1': 72, 'y1': 300,
+        'width': 470, 'height': 30, 'font_size': 12, 'font_family': 'Arial', 'color': (0, 0, 0)},
 
     {'page_number': 4, 'coordinate_name': '', 'x1': 0, 'y1': 0, 'width': 0,
         'height': 0, 'font_size': 0, 'font_family': '', 'color': ()},
 
-    {'page_number': 5, 'coordinate_name': 'beneficiary_name', 'x1': 72, 'y1': 478,
-        'width': 468, 'height': 15, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 5, 'coordinate_name': 'IMG_beneficiary_stamp', 'x1': 72, 'y1': 511,
-        'width': 115, 'height': 264, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 5, 'coordinate_name': 'beneficiary_name', 'x1': 72, 'y1': 450,
+        'width': 470, 'height': 20, 'font_size': 12, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 5, 'coordinate_name': 'IMG_beneficiary_stamp', 'x1': 72, 'y1': 475,
+        'width': 120, 'height': 95, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
 
-    {'page_number': 6, 'coordinate_name': 'date', 'x1': 468, 'y1': 100, 'width': 75,
-        'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'account_number', 'x1': 138, 'y1': 244,
-        'width': 78, 'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'reference', 'x1': 216, 'y1': 334, 'width': 129,
-        'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'date', 'x1': 396, 'y1': 334, 'width': 57,
-        'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'currency', 'x1': 90, 'y1': 363, 'width': 39,
-        'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'amount_in_figures', 'x1': 367, 'y1': 360,
-        'width': 68, 'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'currency', 'x1': 443, 'y1': 360, 'width': 35,
-        'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'amount_in_words', 'x1': 90, 'y1': 378,
-        'width': 338, 'height': 14, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'beneficiary_name', 'x1': 176, 'y1': 457,
-        'width': 302, 'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'beneficiary_address', 'x1': 68, 'y1': 469,
-        'width': 234, 'height': 17, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'country', 'x1': 356, 'y1': 471, 'width': 144,
-        'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'beneficiary_bank', 'x1': 136, 'y1': 491,
-        'width': 166, 'height': 9, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'country', 'x1': 356, 'y1': 489, 'width': 144,
-        'height': 21, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)},
-    {'page_number': 6, 'coordinate_name': 'date', 'x1': 486, 'y1': 522, 'width': 54,
-        'height': 11, 'font_size': 8, 'font_family': 'Arial', 'color': (0, 0, 0)}
+    {'page_number': 6, 'coordinate_name': 'date', 'x1': 490, 'y1': 95, 'width': 75,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'account_number', 'x1': 140, 'y1': 240,
+        'width': 100, 'height': 15, 'font_size': 11, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'reference', 'x1': 215, 'y1': 330, 'width': 140,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'date', 'x1': 400, 'y1': 330, 'width': 60,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'currency', 'x1': 90, 'y1': 360, 'width': 35,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'amount_in_figures', 'x1': 365, 'y1': 365,
+        'width': 70, 'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'currency', 'x1': 440, 'y1': 365, 'width': 50,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'amount_in_words', 'x1': 65, 'y1': 378,
+        'width': 365, 'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'beneficiary_name', 'x1': 190, 'y1': 450,
+        'width': 380, 'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'beneficiary_address', 'x1': 70, 'y1': 470,
+        'width': 245, 'height': 18, 'font_size': 7, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'country', 'x1': 355, 'y1': 470, 'width': 140,
+        'height': 15, 'font_size': 11, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'beneficiary_bank', 'x1': 135, 'y1': 488,
+        'width': 180, 'height': 12, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'country', 'x1': 355, 'y1': 485, 'width': 140,
+        'height': 15, 'font_size': 11, 'font_family': 'Arial', 'color': (0, 0, 0)},
+    {'page_number': 6, 'coordinate_name': 'date', 'x1': 485, 'y1': 520, 'width': 75,
+        'height': 15, 'font_size': 10, 'font_family': 'Arial', 'color': (0, 0, 0)}
 ]
 
 
@@ -152,8 +152,33 @@ def pdfGenerator(request, pdf_doc, page_details):
                 del image
             else:
                 field_value = request.POST.get(field_name, '')
-                # print(x1, y1, width, height)
-                page.insert_textbox(box, field_value)
+                
+                if field_name == "amount_in_figures":
+                    amount = int(field_value)
+                    field_value = '{:,}'.format(amount).replace(',', ' ')
+                    
+                if page_number == 3:
+                    if field_name == "beneficiary_name":
+                        field_value += " en ses bureaux sis Business"
+                    else:
+                        field_value += " (hereinafter called “Partenaire”)"
+
+                if page_number == 5 and field_name == "beneficiary_name":
+                    field_value += ", DIRECTEUR"
+
+                if field_name == "account_number" and  page_number == 1:
+                    field_value = " "+field_value[0]+"   "+field_value[1]+"   "+field_value[2]
+                if field_name == "account_number" and  page_number == 6:
+                    field_value = "301090012" + field_value
+
+                ## For creating a shape(can be used to test the width of a textbox)
+                # shape = page.new_shape()  # create Shape
+                # shape.draw_rect(box)  # draw rectangles
+                # shape.finish(fill = (1,0,1))
+                # shape.commit()
+
+                options = {"fontname" : "Times-Roman", "fontsize" : font_size, "color" : color, "align": 0}
+                page.insert_textbox(box, field_value, **options)
 
     path, fileName = createFilePathAndFileName()
 
